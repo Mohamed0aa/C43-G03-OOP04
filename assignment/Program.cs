70f87ecb1581f1ca4c6624e28bcd9fc3785e94a0
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-09 00:52:16 +0200

    part1

---
+++ assignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region part1
+            #region q1 :What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            #endregion
+            #region q2 :Which of the following is NOT a valid access modifier for interface members in C#?
+            //a) private
+            #endregion
+            #region q3 :Can an interface contain fields in C#?
+            //NO
+            #endregion
+            #region q4 :In C#, can an interface inherit from another interface?
+
+            //yes
+            #endregion
+
+            #region q5 :Which keyword is used to implement an interface in a class in C#?
+            //d) implements
+            #endregion
+            #region q6 : Can an interface contain static methods in C#?
+            // yes
+            #endregion
+            #region q7 :In C#, can an interface have explicit access modifiers for its members?
+
+            //b) No, all members are implicitly public
+            #endregion
+            #region q8 :What is the purpose of an explicit interface implementation in C#?
+            //b) To provide a clear separation between interface and class members
+            #endregion
+            #region q9 : In C#, can an interface have a constructor?
+            //b) No, interfaces cannot have constructors
+            #endregion
+            #region q10 :How can a C# class implement multiple interfaces?
+            // c) By separating interface names with commas
+            #endregion 
+            #endregion
+        }
+    }
+}
