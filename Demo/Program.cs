325adf5af47ae8882cb8210efd4aaf9904d5d136
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-08 16:02:36 +0200

    v2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,13 +2,28 @@
 {
     internal class Program
     {
+        static void print(Iseries seriesss)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                seriesss.get_next();
+                Console.WriteLine(seriesss.current);
+            }
+            seriesss.reset();
+            Console.WriteLine( seriesss.current);
+        }
         static void Main(string[] args)
         {
-            #region vedio 1
+            #region vedio 1  interface ex1
             //    Itype refrence=new typeA();
             //refrence.myproperty = 33;
             //refrence.method1();
             //refrence.method();
+            #endregion
+
+            #region vedio 2 interface ex2
+            Series seriesby2 =new Series();
+            print(seriesby2);
             #endregion
 
         }
---
+++ Demo/Series.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Series : Iseries
+    {
+        public int current { get; set; }
+
+        public void get_next()
+        {
+            current+=2;
+        }
+
+        public void reset()
+        {
+            current=0;
+        }
+    }
+}
